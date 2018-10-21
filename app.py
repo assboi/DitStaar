f1d9e6d0a27a4fa340736bb176755806a8d2222e
asleboon <asle.berge@gmail.com>
2018-10-21 16:19:43 +0200

    Create login from & flask templating

--- app.py
+++ app.py
@@ -1,11 +1,21 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
+app.config["SECRET_KEY"] = "mysecret"
 
 
 @app.route('/')
 def index():
     return render_template("index.html")
+
+
+@app.route('/login', methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        return "success"
+    return render_template("login.html")
+
+
 
 if __name__ == '__main__':
     app.run(debug=True, host="0.0.0.0", port=4000)
