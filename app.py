1df55aa9e5807c66e8eefc0019358148cefe7782
asleboon <asle.berge@gmail.com>
2018-10-22 00:35:18 +0200

    Create demo login

--- app.py
+++ app.py
@@ -1,10 +1,30 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, Response, request, url_for, session, abort, redirect
+from flask_login import LoginManager, UserMixin, login_required, \
+                        login_user, logout_user
 
 app = Flask(__name__)
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = "login"
+
 app.config["SECRET_KEY"] = "mysecret"
+
+class User(UserMixin):
+
+    def __init__(self, id):
+        self.id = id
+        self.name = "user" + str(id)
+        self.password = "secret"
+
+    def __repr__(self):
+        return "%d%s%s" % (self.id, self.name, self.password)
+
+
+user = User(1)
 
 
 @app.route('/')
+@login_required
 def index():
     return render_template("index.html")
 
@@ -12,8 +32,39 @@ def index():
 @app.route('/login', methods=["GET", "POST"])
 def login():
     if request.method == "POST":
-        return "success"
+        username = request.form.get("username")
+        password = request.form.get("password")
+        if password == "secret":
+            id = username.split("user")[1]
+            user = User(id)
+            login_user(user)
+            return redirect(url_for("index"))
+        else:
+            abort(401)
     return render_template("login.html")
+
+
+@app.route("/logout")
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('login'))
+
+# handle login failed
+@app.errorhandler(401)
+def page_not_found(e):
+    return Response('<p>Login failed</p>')
+
+@app.route('/secret')
+@login_required
+def secret():
+    return "you are logged in"
+
+
+@login_manager.user_loader
+def load_user(userid):
+    return User(userid)
+
 
 
 
