21f736cd0ddbb207fe8a06a7abbef31f02d5be42
asleboon <asle.berge@gmail.com>
2018-10-27 15:32:18 +0200

    Fix scrolling on index

--- static/js/script.js
+++ static/js/script.js
@@ -11,41 +11,48 @@ $("#myNavbar a").on('click', function (event) {
         // Using jQuery's animate() method to add smooth page scroll
         // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
         $('html, body').animate({
-            scrollTop: $(hash).offset().top
+            // -50 will account for the sticky header
+            scrollTop: $(hash).offset().top-50
         }, 1000, function () {
 
             // Add hash (#) to URL when done scrolling (default click behavior)
-            window.location.hash = hash;
+            // window.location.hash = hash;
         });
     }
 });
 
-window.onload = function() {
+
+
+
+$(function(){
     var map1 = new google.maps.Map(document.getElementById("map1"),
         {
-            center: { lat: 59.09578, lng: 7.212224},
+            center: { lat: 59.09578, lng: 7.212224 },
             zoom: 12,
             disableDefaultUI: true
 
         });
-        var skrubbsbu = new google.maps.Marker(
-            {
-                position: { lat: 59.09578, lng: 7.212224 },
-                map: map1
-            }
-        );
+    var skrubbsbu = new google.maps.Marker(
+        {
+            position: { lat: 59.09578, lng: 7.212224 },
+            map: map1
+        }
+    );
 
     var map2 = new google.maps.Map(document.getElementById("map2"),
         {
-            center: { lat: 59.125798, lng: 6.992701},
+            center: { lat: 59.125798, lng: 6.992701 },
             zoom: 13,
             disableDefaultUI: true
 
         });
-        var taumevatn = new google.maps.Marker(
-            {
-                position: { lat: 59.125798, lng: 6.992701 },
-                map: map2
-            }
-        );
-}
+    var taumevatn = new google.maps.Marker(
+        {
+            position: { lat: 59.125798, lng: 6.992701 },
+            map: map2
+        }
+    );
+
+}); 
+
+
